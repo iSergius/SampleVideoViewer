f0bde71b9d4f9a80c65e7aa7630d34ad2a5cbabc
Sergey Kondratyev <ron.rcf@gmail.com>
2017-02-22 07:11:02 +0500

    Refactor class EventReadyToReceiveOffer Test cover

---
+++ app/src/main/java/name/isergius/android/task/hazpro/samplevideochat/core/socket/EventReadyToReceiveOffer.java
@@ -0,0 +1,52 @@
+package name.isergius.android.task.hazpro.samplevideochat.core.socket;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.emitter.Emitter;
+import name.isergius.android.task.hazpro.samplevideochat.core.ClientStore;
+import name.isergius.android.task.hazpro.samplevideochat.core.StoreException;
+import name.isergius.android.task.hazpro.samplevideochat.data.Server;
+
+/**
+ * @author Sergey Kondratyev
+ */
+public class EventReadyToReceiveOffer implements Emitter.Listener {
+    public static final String EVENT_ID = "ready_to_receive_offer";
+    public static final String EVENT_ALIAS0 = "client_ready";
+    private static final String TAG = EventReadyToReceiveOffer.class.getSimpleName();
+
+    private ClientStore clientStore;
+
+    public EventReadyToReceiveOffer(ClientStore clientStore) {
+        this.clientStore = clientStore;
+    }
+
+    @Override
+    public void call(Object... args) {
+        Log.v(TAG, EVENT_ID);
+        Log.v(TAG, args[0].toString());
+        JSONObject object = (JSONObject) args[0];
+        try {
+            String clientId = object.getString("clientId");
+            JSONArray iceServers = object
+                    .getJSONObject("iceServers")
+                    .getJSONArray("iceServers");
+            for (int i = 0; i < iceServers.length(); i++) {
+                clientStore.addIceServer(clientId, convertToServer(iceServers.getJSONObject(i)));
+            }
+        } catch (JSONException | StoreException e) {
+            Log.e(TAG, "Error", e);
+        }
+    }
+
+    Server convertToServer(JSONObject server) throws JSONException {
+        String credential = server.getString("credential");
+        String username = server.getString("username");
+        String url = server.getString("url");
+        return new Server(credential, username, url);
+    }
+}
---
+++ app/src/test/java/name/isergius/android/task/hazpro/samplevideochat/core/socket/EventReadyToReceiveOfferTest.java
@@ -0,0 +1,41 @@
+package name.isergius.android.task.hazpro.samplevideochat.core.socket;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import name.isergius.android.task.hazpro.samplevideochat.core.ClientStore;
+import name.isergius.android.task.hazpro.samplevideochat.data.Server;
+
+/**
+ * @author Sergey Kondratyev
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class EventReadyToReceiveOfferTest {
+    @Mock
+    private ClientStore clientStore;
+
+    private JSONObject arg;
+
+    private String JSON = "{\"iceServers\":{\"iceServers\":[{\"url\":\"turn:turn.appear.in:443?transport=udp\",\"urls\":\"turn:turn.appear.in:443?transport=udp\",\"username\":\"appearin:1487381191\",\"credential\":\"j/1YtwTvE6yqlb4l/sbYWW1oA64=\"},{\"url\":\"turn:turn.appear.in:443?transport=tcp\",\"urls\":\"turn:turn.appear.in:443?transport=tcp\",\"username\":\"appearin:1487381191\",\"credential\":\"j/1YtwTvE6yqlb4l/sbYWW1oA64=\"},{\"url\":\"turns:turn.appear.in:443?transport=tcp\",\"urls\":\"turns:turn.appear.in:443?transport=tcp\",\"username\":\"appearin:1487381191\",\"credential\":\"j/1YtwTvE6yqlb4l/sbYWW1oA64=\"}]},\"clientId\":\"6236007a-6651-453b-8701-fe127541e048\",\"deviceId\":\"6a5c7759-0c7b-4ccd-a076-8a4155e58ee3\"}";
+    private EventReadyToReceiveOffer eventReadyToReceiveOffer;
+
+    @Before
+    public void setUp() throws Exception {
+        this.arg = new JSONObject(JSON);
+        this.eventReadyToReceiveOffer = new EventReadyToReceiveOffer(clientStore);
+    }
+
+    @Test
+    public void call() throws Exception {
+        String clientId = "6236007a-6651-453b-8701-fe127541e048";
+        eventReadyToReceiveOffer.call(arg);
+        Mockito.verify(clientStore).addIceServer(clientId, new Server("j/1YtwTvE6yqlb4l/sbYWW1oA64=", "appearin:1487381191", "turn:turn.appear.in:443?transport=udp"));
+    }
+
+}
